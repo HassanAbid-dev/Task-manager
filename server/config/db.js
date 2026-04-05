9c569776aab9da546288c32c6a7dceb9d358e389
Hassan Abid 15 <bsse23015@itu.edu.pk>
2026-04-05 13:35:20 +0500

    First commit

---
+++ server/config/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+export const connectDB = async () => {
+  try {
+    mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
